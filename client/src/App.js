68cc74316ab0563e91422b8da47409bf01170c75
Ismail <ismailmohammedraslan@gmail.com>
2022-03-23 01:49:15 +0200

    stater react project

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import "./index.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{ padding: 20 }}>
+        This text is styled with some of the text formatting properties. The
+        heading uses the text-align, text-transform, and color properties. The
+        paragraph is indented, aligned, and the space between characters is
+        specified. The underline is removed from this colored
+      </h1>
+    </div>
+  );
+}
+
+export default App;
