c80a3f435b4adf2aab37bbcc18e43cfd81b8da3b
Ismail <ismailmohammedraslan@gmail.com>
2022-03-23 15:10:40 +0200

    Add data file and some static words

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
+import { words } from "./staticFile";
 
 function App() {
   return (
     <div className="layout">
       <Header />
-      <main>Content</main>
+      <main>{words.content}</main>
       <Footer />
     </div>
   );
---
+++ client/src/staticFile.js
@@ -0,0 +1,5 @@
+export const words = {
+  headerTitle: "Shopping Card",
+  footerTitle: "Ⓒ copyright Ismail 2022",
+  content: "Content",
+};
