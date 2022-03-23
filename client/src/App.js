536c2e53fb042d83a7c8513ede531556bfe31c6b
Ismail <ismailmohammedraslan@gmail.com>
2022-03-23 02:19:39 +0200

    header comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,11 @@
-import "./index.scss";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1 style={{ padding: 20 }}>
-        This text is styled with some of the text formatting properties. The
-        heading uses the text-align, text-transform, and color properties. The
-        paragraph is indented, aligned, and the space between characters is
-        specified. The underline is removed from this colored
-      </h1>
+    <div className="layout">
+      <Header />
+      <main>Content</main>
+      <footer>Footer</footer>
     </div>
   );
 }
