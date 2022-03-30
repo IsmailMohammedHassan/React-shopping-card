45563838498463a053b47116e3c58f51e7d6e0f6
Ismail <ismailmohammedraslan@gmail.com>
2022-03-30 19:36:18 +0200

    add redux product

--- client/src/Components/Products/Products.jsx
+++ client/src/Components/Products/Products.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../../css/Products/Products.css";
 import ProductModal from "./ProductModal/ProductModal";
 import Zoom from "react-reveal/Zoom";
+import { connect } from "react-redux";
+import { fetchProducts } from "../../Store/Actions/Product";
 
-export default function Products({ products, addToCard }) {
+function Products({ products, addToCard, fetchProducts }) {
   const [product, setProduct] = useState("");
 
   const openModal = (prod) => {
@@ -13,25 +15,40 @@ export default function Products({ products, addToCard }) {
     setProduct(false);
   };
 
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <Zoom cascade>
       <div className="products-wrapper">
-        {products.map((prod) => (
-          <div key={prod.id} className="product-item">
-            <a href="#" onClick={() => openModal(prod)}>
-              <img src={prod.imageUrl} alt={prod.title} />
-            </a>
-            <div className="product-desc">
-              <p>{prod.title}</p>
-              <p>{prod.price}</p>
-            </div>
-            <button onClick={() => addToCard(prod)} className="Btn">
-              Add to card
-            </button>
-          </div>
-        ))}
+        {products && products.length
+          ? products.map((prod) => (
+              <div key={prod.id} className="product-item">
+                <a href="#" onClick={() => openModal(prod)}>
+                  <img src={prod.imageUrl} alt={prod.title} />
+                </a>
+                <div className="product-desc" key={prod.id}>
+                  <p>{prod.title}</p>
+                  <p>{prod.price}</p>
+                </div>
+                <button onClick={() => addToCard(prod)} className="Btn">
+                  Add to card
+                </button>
+              </div>
+            ))
+          : "Loading...."}
         <ProductModal product={product} closeModal={closeModal} />
       </div>
     </Zoom>
   );
 }
+
+export default connect(
+  (state) => {
+    return {
+      products: state.Products.products,
+    };
+  },
+  { fetchProducts }
+)(Products);
